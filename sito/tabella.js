7928fc4b591f60b0180c57fa0248611779657422
dadodyj <104713454+dadodyj@users.noreply.github.com>
2024-09-28 09:34:46 +0200

    Delete sito directory

--- sito/tabella.js
+++
@@ -1,5 +0,0 @@
-function show(){
-    document.getElementById("image").style.display = "block";
-    document.getElementById("imgbtn").style.display = "none";
-    document.getElementById("titolo").style.display = "none";
-}
