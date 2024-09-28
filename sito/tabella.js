83cdf5968e36b1dde169b8f41a6671df4405a87f
dadodyj <104713454+dadodyj@users.noreply.github.com>
2024-09-28 09:28:50 +0200

    Add files via upload

---
+++ sito/tabella.js
@@ -0,0 +1,5 @@
+function show(){
+    document.getElementById("image").style.display = "block";
+    document.getElementById("imgbtn").style.display = "none";
+    document.getElementById("titolo").style.display = "none";
+}
